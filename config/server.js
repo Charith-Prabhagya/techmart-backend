49d6a46d016c15713c74eba88ae0c95baee987ae
cprabhagya@gmail.com <cprabhagya@gmail.com>
2021-10-13 13:50:08 +0530

    uploading project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cf50f836baf3aa09efeb50af67759b92'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7a4a510a-0ab6-4c9b-8cc3-9c42d3dcf2ae'
+};
